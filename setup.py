f1cc90167b8e17d8bd2205513e10fc4aa001384c
Kylo P <100837335+cywf@users.noreply.github.com>
2024-11-16 21:11:14 -0400

    updated setup script

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
 from setuptools import setup, find_packages
 
+# Function to read the requirements.txt file
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    return [line.strip() for line in lines if line and not line.startswith('#')]
+
 setup(
     name='pr_cybr_agent',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=[
-        # Add dependencies from requirements.txt
-    ],
+    install_requires=parse_requirements('requirements.txt'),
     author='PR-CYBR',
     author_email='support@pr-cybr.com',
     description='PR-CYBR Agent',
     url='https://github.com/pr-cybr/',
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.8',
 )
