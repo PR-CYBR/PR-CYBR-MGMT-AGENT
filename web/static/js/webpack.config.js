50fd280ec172d47935cd7ebec7139fa956e38860
Kylo P <100837335+cywf@users.noreply.github.com>
2024-11-19 15:29:25 -0400

    updated webpack script

--- web/static/js/webpack.config.js
+++ web/static/js/webpack.config.js
@@ -4,10 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production', // Set to 'production' for optimized builds
-  entry: path.resolve(__dirname, 'status-page.js'), // Adjusted entry point path
+  entry: path.resolve(__dirname, 'status-page.js'), // Ensure this path is correct
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '../dist'), // Adjusted output directory path
+    path: path.resolve(__dirname, '../../dist'), // Adjusted output directory path to be two levels up
   },
   module: {
     rules: [
@@ -29,12 +29,14 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../templates/status.html'), // Adjusted template path
+      template: path.resolve(__dirname, '../templates/status.html'), // Ensure this path is correct
       filename: 'index.html', // Output HTML file
     }),
   ],
   devServer: {
-    contentBase: path.resolve(__dirname, '../dist'), // Adjusted content base path
+    static: {
+      directory: path.resolve(__dirname, '../../dist'), // Updated for Webpack 5
+    },
     compress: true,
     port: 9000,
   },
