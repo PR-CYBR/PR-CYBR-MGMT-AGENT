2a8a0d0332e8aaa3cb6a1d9b273ca07358d30e08
Kylo P <100837335+cywf@users.noreply.github.com>
2024-11-19 20:55:01 -0400

    updated webpack

--- src/js/webpack.config.js
+++ src/js/webpack.config.js
@@ -4,10 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: path.resolve(__dirname, 'src/js/main.js'),
+  entry: path.resolve(__dirname, '../static/js/main.js'), // Corrected entry point path
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/'),
+    path: path.resolve(__dirname, '../../dist/'), // Ensure this path is correct
   },
   module: {
     rules: [
@@ -29,13 +29,13 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/templates/index.html'),
+      template: path.resolve(__dirname, '../templates/index.html'), // Corrected template path
       filename: 'index.html',
     }),
   ],
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist/'),
+      directory: path.resolve(__dirname, '../../dist/'), // Ensure this path is correct
     },
     compress: true,
     port: 9000,
